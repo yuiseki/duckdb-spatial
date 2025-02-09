0cb7c16f8c06a95c2b9eab2f3b36e9b365276ae9
Max Gabrielsson <max@gabrielsson.com>
2025-02-09 20:37:15 +0100

    use approx bounding box in spatial_optimizers

--- src/spatial/spatial_optimizers.cpp
+++ src/spatial/spatial_optimizers.cpp
@@ -99,172 +99,184 @@ public:
 				                                     "st_within",    "st_contains",        "st_overlaps", "st_covers",
 				                                     "st_coveredby", "st_containsproperly"};
 
-				if (predicates.find(bound_function.function.name) != predicates.end()) {
-					// Found a spatial predicate we can optimize
-
-					// Convert this into a comparison join on st_xmin, st_xmax, st_ymin, st_ymax of the two input
-					// geometries
-					auto left_pred_expr = std::move(bound_function.children[0]);
-					auto right_pred_expr = std::move(bound_function.children[1]);
-
-					// We need to place the left side of the predicate on the left side of the join
-					// and the right side of the predicate on the right side of the join
-					// So look at the table indexes of the left and right side of the predicate
-					unordered_set<idx_t> left_table_indexes;
-					LogicalJoin::GetTableReferences(*any_join.children[0], left_table_indexes);
-
-					unordered_set<idx_t> right_table_indexes;
-					LogicalJoin::GetTableReferences(*any_join.children[1], right_table_indexes);
-
-					unordered_set<idx_t> left_pred_bindings;
-					LogicalJoin::GetExpressionBindings(*left_pred_expr, left_pred_bindings);
-
-					unordered_set<idx_t> right_pred_bindings;
-					LogicalJoin::GetExpressionBindings(*right_pred_expr, right_pred_bindings);
-
-					// Check if we can optimize this join
-					// We need to make sure that the left and right side of the predicate are disjoint
-					// e.g.
-					// 		a JOIN b ON st_intersects(a.geom, b.geom) 						=> OK
-					//		a JOIN b ON st_intersects(b.geom, a.geom)				 		=> OK
-					//		a JOIN b ON st_intersects(a.geom, st_union(a.geom, b.geom)) 	=> NOT OK
-					auto can_split = IsTableRefsDisjoint(left_table_indexes, right_table_indexes, left_pred_bindings,
-					                                     right_pred_bindings);
-					if (!can_split) {
-						// Try again with the left and right side of the predicate swapped
-						// We can safely swap because the intersection operation we encode with the comparison join
-						// is symmetric, so the order of the arguments wont matter in the "new" join condition we're
-						// about to create.
-						can_split = IsTableRefsDisjoint(left_table_indexes, right_table_indexes, right_pred_bindings,
-						                                left_pred_bindings);
-						if (!can_split) {
-							// We cant optimize this join
-							return;
-						}
-						// Swap the left and right side of the predicate
-						std::swap(left_pred_expr, right_pred_expr);
-					}
-
-					// Lookup the st_xmin, st_xmax, st_ymin, st_ymax functions in the catalog
-					auto &catalog = Catalog::GetSystemCatalog(context);
-
-					auto &extent_func_set =
-					    catalog.GetEntry(context, CatalogType::SCALAR_FUNCTION_ENTRY, DEFAULT_SCHEMA, "st_extent")
-					        .Cast<ScalarFunctionCatalogEntry>();
-
-					auto &xmin_func_set =
-					    catalog.GetEntry(context, CatalogType::SCALAR_FUNCTION_ENTRY, DEFAULT_SCHEMA, "st_xmin")
-					        .Cast<ScalarFunctionCatalogEntry>();
-					auto &xmax_func_set =
-					    catalog.GetEntry(context, CatalogType::SCALAR_FUNCTION_ENTRY, DEFAULT_SCHEMA, "st_xmax")
-					        .Cast<ScalarFunctionCatalogEntry>();
-					auto &ymin_func_set =
-					    catalog.GetEntry(context, CatalogType::SCALAR_FUNCTION_ENTRY, DEFAULT_SCHEMA, "st_ymin")
-					        .Cast<ScalarFunctionCatalogEntry>();
-					auto &ymax_func_set =
-					    catalog.GetEntry(context, CatalogType::SCALAR_FUNCTION_ENTRY, DEFAULT_SCHEMA, "st_ymax")
-					        .Cast<ScalarFunctionCatalogEntry>();
-
-					auto &left_arg_type = left_pred_expr->return_type;
-					auto &right_arg_type = right_pred_expr->return_type;
-
-					auto extent_func_left = extent_func_set.functions.GetFunctionByArguments(context, {left_arg_type});
-					auto extent_func_right =
-					    extent_func_set.functions.GetFunctionByArguments(context, {right_arg_type});
-
-					auto xmin_func_left =
-					    xmin_func_set.functions.GetFunctionByArguments(context, {extent_func_left.return_type});
-					auto xmax_func_left =
-					    xmax_func_set.functions.GetFunctionByArguments(context, {extent_func_left.return_type});
-					auto ymin_func_left =
-					    ymin_func_set.functions.GetFunctionByArguments(context, {extent_func_left.return_type});
-					auto ymax_func_left =
-					    ymax_func_set.functions.GetFunctionByArguments(context, {extent_func_left.return_type});
-
-					auto xmin_func_right =
-					    xmin_func_set.functions.GetFunctionByArguments(context, {extent_func_right.return_type});
-					auto xmax_func_right =
-					    xmax_func_set.functions.GetFunctionByArguments(context, {extent_func_right.return_type});
-					auto ymin_func_right =
-					    ymin_func_set.functions.GetFunctionByArguments(context, {extent_func_right.return_type});
-					auto ymax_func_right =
-					    ymax_func_set.functions.GetFunctionByArguments(context, {extent_func_right.return_type});
-
-					// Create the new join condition
-					vector<unique_ptr<Expression>> left_extent_args;
-					left_extent_args.push_back(left_pred_expr->Copy());
-					auto left_extent = make_uniq<BoundFunctionExpression>(
-					    GeoTypes::BOX_2D(), std::move(extent_func_left), std::move(left_extent_args), nullptr);
-
-					vector<unique_ptr<Expression>> right_extent_args;
-					right_extent_args.push_back(right_pred_expr->Copy());
-					auto right_extent = make_uniq<BoundFunctionExpression>(
-					    GeoTypes::BOX_2D(), std::move(extent_func_right), std::move(right_extent_args), nullptr);
-
-					// Left
-					vector<unique_ptr<Expression>> left_xmin_args;
-					left_xmin_args.push_back(left_extent->Copy());
-					auto a_x_min = make_uniq<BoundFunctionExpression>(LogicalType::DOUBLE, std::move(xmin_func_left),
-					                                                  std::move(left_xmin_args), nullptr);
-
-					vector<unique_ptr<Expression>> left_xmax_args;
-					left_xmax_args.push_back(left_extent->Copy());
-					auto a_x_max = make_uniq<BoundFunctionExpression>(LogicalType::DOUBLE, std::move(xmax_func_left),
-					                                                  std::move(left_xmax_args), nullptr);
-
-					vector<unique_ptr<Expression>> left_ymin_args;
-					left_ymin_args.push_back(left_extent->Copy());
-					auto a_y_min = make_uniq<BoundFunctionExpression>(LogicalType::DOUBLE, std::move(ymin_func_left),
-					                                                  std::move(left_ymin_args), nullptr);
-
-					vector<unique_ptr<Expression>> left_ymax_args;
-					left_ymax_args.push_back(left_extent->Copy());
-					auto a_y_max = make_uniq<BoundFunctionExpression>(LogicalType::DOUBLE, std::move(ymax_func_left),
-					                                                  std::move(left_ymax_args), nullptr);
-
-					// Right
-					vector<unique_ptr<Expression>> right_xmin_args;
-					right_xmin_args.push_back(right_extent->Copy());
-					auto b_x_min = make_uniq<BoundFunctionExpression>(LogicalType::DOUBLE, std::move(xmin_func_right),
-					                                                  std::move(right_xmin_args), nullptr);
-
-					vector<unique_ptr<Expression>> right_xmax_args;
-					right_xmax_args.push_back(right_extent->Copy());
-					auto b_x_max = make_uniq<BoundFunctionExpression>(LogicalType::DOUBLE, std::move(xmax_func_right),
-					                                                  std::move(right_xmax_args), nullptr);
-
-					vector<unique_ptr<Expression>> right_ymin_args;
-					right_ymin_args.push_back(right_extent->Copy());
-					auto b_y_min = make_uniq<BoundFunctionExpression>(LogicalType::DOUBLE, std::move(ymin_func_right),
-					                                                  std::move(right_ymin_args), nullptr);
-
-					vector<unique_ptr<Expression>> right_ymax_args;
-					right_ymax_args.push_back(right_extent->Copy());
-					auto b_y_max = make_uniq<BoundFunctionExpression>(LogicalType::DOUBLE, std::move(ymax_func_right),
-					                                                  std::move(right_ymax_args), nullptr);
-
-					// Now create the new join operator
-					auto new_join = make_uniq<LogicalComparisonJoin>(JoinType::INNER);
-					AddComparison(new_join, std::move(a_x_min), std::move(b_x_max),
-					              ExpressionType::COMPARE_LESSTHANOREQUALTO);
-					AddComparison(new_join, std::move(a_x_max), std::move(b_x_min),
-					              ExpressionType::COMPARE_GREATERTHANOREQUALTO);
-					AddComparison(new_join, std::move(a_y_min), std::move(b_y_max),
-					              ExpressionType::COMPARE_LESSTHANOREQUALTO);
-					AddComparison(new_join, std::move(a_y_max), std::move(b_y_min),
-					              ExpressionType::COMPARE_GREATERTHANOREQUALTO);
-
-					new_join->children = std::move(any_join.children);
-					if (any_join.has_estimated_cardinality) {
-						new_join->estimated_cardinality = any_join.estimated_cardinality;
-						new_join->has_estimated_cardinality = true;
-					}
-
-					auto filter = make_uniq<LogicalFilter>(std::move(any_join.condition));
-					filter->children.push_back(std::move(new_join));
-
-					plan = std::move(filter);
+				if (predicates.find(bound_function.function.name) == predicates.end()) {
+					return;
 				}
+
+				if(bound_function.children.size() != 2) {
+					return;
+				}
+
+				// It has to be on raw geometry types
+				if(bound_function.children[0]->return_type != GeoTypes::GEOMETRY() ||
+				   bound_function.children[1]->return_type != GeoTypes::GEOMETRY()) {
+					return;
+				}
+
+				// Found a spatial predicate we can optimize
+
+				// Convert this into a comparison join on st_xmin, st_xmax, st_ymin, st_ymax of the two input
+				// geometries
+				auto left_pred_expr = std::move(bound_function.children[0]);
+				auto right_pred_expr = std::move(bound_function.children[1]);
+
+				// We need to place the left side of the predicate on the left side of the join
+				// and the right side of the predicate on the right side of the join
+				// So look at the table indexes of the left and right side of the predicate
+				unordered_set<idx_t> left_table_indexes;
+				LogicalJoin::GetTableReferences(*any_join.children[0], left_table_indexes);
+
+				unordered_set<idx_t> right_table_indexes;
+				LogicalJoin::GetTableReferences(*any_join.children[1], right_table_indexes);
+
+				unordered_set<idx_t> left_pred_bindings;
+				LogicalJoin::GetExpressionBindings(*left_pred_expr, left_pred_bindings);
+
+				unordered_set<idx_t> right_pred_bindings;
+				LogicalJoin::GetExpressionBindings(*right_pred_expr, right_pred_bindings);
+
+				// Check if we can optimize this join
+				// We need to make sure that the left and right side of the predicate are disjoint
+				// e.g.
+				// 		a JOIN b ON st_intersects(a.geom, b.geom) 						=> OK
+				//		a JOIN b ON st_intersects(b.geom, a.geom)				 		=> OK
+				//		a JOIN b ON st_intersects(a.geom, st_union(a.geom, b.geom)) 	=> NOT OK
+				auto can_split = IsTableRefsDisjoint(left_table_indexes, right_table_indexes, left_pred_bindings,
+				                                     right_pred_bindings);
+				if (!can_split) {
+					// Try again with the left and right side of the predicate swapped
+					// We can safely swap because the intersection operation we encode with the comparison join
+					// is symmetric, so the order of the arguments wont matter in the "new" join condition we're
+					// about to create.
+					can_split = IsTableRefsDisjoint(left_table_indexes, right_table_indexes, right_pred_bindings,
+					                                left_pred_bindings);
+					if (!can_split) {
+						// We cant optimize this join
+						return;
+					}
+					// Swap the left and right side of the predicate
+					std::swap(left_pred_expr, right_pred_expr);
+				}
+
+				// Lookup the st_xmin, st_xmax, st_ymin, st_ymax functions in the catalog
+				auto &catalog = Catalog::GetSystemCatalog(context);
+
+				auto &extent_func_set =
+				    catalog.GetEntry(context, CatalogType::SCALAR_FUNCTION_ENTRY, DEFAULT_SCHEMA, "st_extent_approx")
+				        .Cast<ScalarFunctionCatalogEntry>();
+
+				auto &xmin_func_set =
+				    catalog.GetEntry(context, CatalogType::SCALAR_FUNCTION_ENTRY, DEFAULT_SCHEMA, "st_xmin")
+				        .Cast<ScalarFunctionCatalogEntry>();
+				auto &xmax_func_set =
+				    catalog.GetEntry(context, CatalogType::SCALAR_FUNCTION_ENTRY, DEFAULT_SCHEMA, "st_xmax")
+				        .Cast<ScalarFunctionCatalogEntry>();
+				auto &ymin_func_set =
+				    catalog.GetEntry(context, CatalogType::SCALAR_FUNCTION_ENTRY, DEFAULT_SCHEMA, "st_ymin")
+				        .Cast<ScalarFunctionCatalogEntry>();
+				auto &ymax_func_set =
+				    catalog.GetEntry(context, CatalogType::SCALAR_FUNCTION_ENTRY, DEFAULT_SCHEMA, "st_ymax")
+				        .Cast<ScalarFunctionCatalogEntry>();
+
+				auto &left_arg_type = left_pred_expr->return_type;
+				auto &right_arg_type = right_pred_expr->return_type;
+
+				auto extent_func_left = extent_func_set.functions.GetFunctionByArguments(context, {left_arg_type});
+				auto extent_func_right =
+				    extent_func_set.functions.GetFunctionByArguments(context, {right_arg_type});
+
+				auto xmin_func_left =
+				    xmin_func_set.functions.GetFunctionByArguments(context, {extent_func_left.return_type});
+				auto xmax_func_left =
+				    xmax_func_set.functions.GetFunctionByArguments(context, {extent_func_left.return_type});
+				auto ymin_func_left =
+				    ymin_func_set.functions.GetFunctionByArguments(context, {extent_func_left.return_type});
+				auto ymax_func_left =
+				    ymax_func_set.functions.GetFunctionByArguments(context, {extent_func_left.return_type});
+
+				auto xmin_func_right =
+				    xmin_func_set.functions.GetFunctionByArguments(context, {extent_func_right.return_type});
+				auto xmax_func_right =
+				    xmax_func_set.functions.GetFunctionByArguments(context, {extent_func_right.return_type});
+				auto ymin_func_right =
+				    ymin_func_set.functions.GetFunctionByArguments(context, {extent_func_right.return_type});
+				auto ymax_func_right =
+				    ymax_func_set.functions.GetFunctionByArguments(context, {extent_func_right.return_type});
+
+				// Create the new join condition
+				vector<unique_ptr<Expression>> left_extent_args;
+				left_extent_args.push_back(left_pred_expr->Copy());
+				auto left_extent = make_uniq<BoundFunctionExpression>(
+				    GeoTypes::BOX_2D(), std::move(extent_func_left), std::move(left_extent_args), nullptr);
+
+				vector<unique_ptr<Expression>> right_extent_args;
+				right_extent_args.push_back(right_pred_expr->Copy());
+				auto right_extent = make_uniq<BoundFunctionExpression>(
+				    GeoTypes::BOX_2D(), std::move(extent_func_right), std::move(right_extent_args), nullptr);
+
+				// Left
+				vector<unique_ptr<Expression>> left_xmin_args;
+				left_xmin_args.push_back(left_extent->Copy());
+				auto a_x_min = make_uniq<BoundFunctionExpression>(LogicalType::DOUBLE, std::move(xmin_func_left),
+				                                                  std::move(left_xmin_args), nullptr);
+
+				vector<unique_ptr<Expression>> left_xmax_args;
+				left_xmax_args.push_back(left_extent->Copy());
+				auto a_x_max = make_uniq<BoundFunctionExpression>(LogicalType::DOUBLE, std::move(xmax_func_left),
+				                                                  std::move(left_xmax_args), nullptr);
+
+				vector<unique_ptr<Expression>> left_ymin_args;
+				left_ymin_args.push_back(left_extent->Copy());
+				auto a_y_min = make_uniq<BoundFunctionExpression>(LogicalType::DOUBLE, std::move(ymin_func_left),
+				                                                  std::move(left_ymin_args), nullptr);
+
+				vector<unique_ptr<Expression>> left_ymax_args;
+				left_ymax_args.push_back(left_extent->Copy());
+				auto a_y_max = make_uniq<BoundFunctionExpression>(LogicalType::DOUBLE, std::move(ymax_func_left),
+				                                                  std::move(left_ymax_args), nullptr);
+
+				// Right
+				vector<unique_ptr<Expression>> right_xmin_args;
+				right_xmin_args.push_back(right_extent->Copy());
+				auto b_x_min = make_uniq<BoundFunctionExpression>(LogicalType::DOUBLE, std::move(xmin_func_right),
+				                                                  std::move(right_xmin_args), nullptr);
+
+				vector<unique_ptr<Expression>> right_xmax_args;
+				right_xmax_args.push_back(right_extent->Copy());
+				auto b_x_max = make_uniq<BoundFunctionExpression>(LogicalType::DOUBLE, std::move(xmax_func_right),
+				                                                  std::move(right_xmax_args), nullptr);
+
+				vector<unique_ptr<Expression>> right_ymin_args;
+				right_ymin_args.push_back(right_extent->Copy());
+				auto b_y_min = make_uniq<BoundFunctionExpression>(LogicalType::DOUBLE, std::move(ymin_func_right),
+				                                                  std::move(right_ymin_args), nullptr);
+
+				vector<unique_ptr<Expression>> right_ymax_args;
+				right_ymax_args.push_back(right_extent->Copy());
+				auto b_y_max = make_uniq<BoundFunctionExpression>(LogicalType::DOUBLE, std::move(ymax_func_right),
+				                                                  std::move(right_ymax_args), nullptr);
+
+				// Now create the new join operator
+				auto new_join = make_uniq<LogicalComparisonJoin>(JoinType::INNER);
+				AddComparison(new_join, std::move(a_x_min), std::move(b_x_max),
+				              ExpressionType::COMPARE_LESSTHANOREQUALTO);
+				AddComparison(new_join, std::move(a_x_max), std::move(b_x_min),
+				              ExpressionType::COMPARE_GREATERTHANOREQUALTO);
+				AddComparison(new_join, std::move(a_y_min), std::move(b_y_max),
+				              ExpressionType::COMPARE_LESSTHANOREQUALTO);
+				AddComparison(new_join, std::move(a_y_max), std::move(b_y_min),
+				              ExpressionType::COMPARE_GREATERTHANOREQUALTO);
+
+				new_join->children = std::move(any_join.children);
+				if (any_join.has_estimated_cardinality) {
+					new_join->estimated_cardinality = any_join.estimated_cardinality;
+					new_join->has_estimated_cardinality = true;
+				}
+
+				auto filter = make_uniq<LogicalFilter>(std::move(any_join.condition));
+				filter->children.push_back(std::move(new_join));
+
+				plan = std::move(filter);
 			}
 		}
 	}
