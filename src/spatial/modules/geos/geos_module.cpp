99cedb256195df145344b7ade4187ad01e624d76
Max Gabrielsson <max@gabrielsson.com>
2025-04-03 19:21:04 +0200

    reorder funcs

--- src/spatial/modules/geos/geos_module.cpp
+++ src/spatial/modules/geos/geos_module.cpp
@@ -297,9 +297,9 @@ struct ST_Buffer {
 
 	    `num_triangles` represents how many triangles that will be produced to approximate a quarter circle. The larger the number, the smoother the resulting geometry. The default value is 8.
 
-	    `join_style` must be one of "JOIN_ROUND", "JOIN_MITRE", "JOIN_BEVEL". This parameter is case-insensitive.
+		`cap_style` must be one of "CAP_ROUND", "CAP_FLAT", "CAP_SQUARE". This parameter is case-insensitive.
 
-	    `cap_style` must be one of "CAP_ROUND", "CAP_FLAT", "CAP_SQUARE". This parameter is case-insensitive.
+	    `join_style` must be one of "JOIN_ROUND", "JOIN_MITRE", "JOIN_BEVEL". This parameter is case-insensitive.
 
 	    `mitre_limit` only applies when `join_style` is "JOIN_MITRE". It is the ratio of the distance from the corner to the mitre point to the corner radius. The default value is 1.0.
 
